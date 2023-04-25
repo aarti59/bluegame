9b665dcbc128e732aaa57a0fa54aadda229e3d81
odin <odin@aistechnolans.org>
2023-04-25 18:26:49 +0530

    s

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <!-- Your app content here -->
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import HelloWorld from "./components/HelloWorld.vue";
+// import store from "./store";
+// import router from "./router";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  // setup() {
+  //   return {
+  //     store,
+  //     router,
+  //   };
+  // },
+});
+</script>
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+// // Element UI
+// import ElementUI from "element-ui";
+// import "@/assets/styles/element.scss";
+// Vue.use(ElementUI);
+
+//styles
+// import "@/assets/styles/app.scss";
+// Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
