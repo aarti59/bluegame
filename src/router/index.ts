77a1bfe0842777c2be04ca7f70c0a76a4a7b5f32
odin <odin@aistechnolans.org>
2023-04-27 11:18:29 +0530

    changed name of comp

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: Login
+      component: SignUp
     },
     {
       path: '/displayname',
